cae7346a679f444da57c9a111da63c08246928b1
Ambily Menon <amen0007@Synchronoss.com>
2018-08-06 13:59:59 +1000

    Load map component

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as ol from 'ol';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  map:Map;
+  source:XYZ;
+  layer:TileLayer;
+  view:View;
+
+  constructor() {
+  }
+
+
+  ngOnInit() {
+    this.source = new XYZ ({
+      // Tiles from Mapbox (Light)
+      url: 'https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    });
+   
+
+    this.layer = new TileLayer({
+      source: this.source
+    });
+
+    this.view = new View({
+      center: [6.661594, 50.433237],
+      zoom: 3,
+    });
+
+    this.map = new Map({
+      target: 'map',
+      layers: [this.layer],
+      view: this.view
+    });
+  }
+
+}
