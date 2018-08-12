d422b6903dac07e650eecb009e0eb45e242a9d78
Ambily Menon <amen0007@Synchronoss.com>
2018-08-12 20:05:51 +1000

    toggle button to show hide nav list

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -11,11 +11,22 @@ import { map } from 'rxjs/operators';
 })
 export class SideNavComponent {
 
+  public myValue = false;
+  public otherValue = false;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  toggleNav(value){
+    console.log("value is" +value);
+    this.myValue=true;
+    console.log(this.myValue);
+    this.otherValue=true;
+  //show different panelsets based on value chosen
+  }
   
   }
