178e0e66883821e301fc08434e3699016fe28f2f
amen0007 <ambily.menon@synchronoss.com>
2018-10-11 15:04:08 +1100

    Adding observable to emit data from service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -2,19 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from  '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Location } from './location';
+import {Address} from './address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  private searchLatLongUrl  =  'https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=-34.44076&lon=-58.70521';
+  private searchLatLongUrl  =  'https://maps.googleapis.com/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBSNv7ia8TfVCFeMSdUyCpRXJNPnxP5XUo';
 
-  constructor(private  httpClient:  HttpClient) { }
+    constructor(private  httpClient:  HttpClient) { }
 
-  
+
 
   getLatLongResults (): Observable<Location> {
-    return this.httpClient.get<Location>(this.searchLatLongUrl)
+   // return this.httpClient.get<Location>(this.searchLatLongUrl)
+    return null;
+  }
+
+  getAddress(): Observable<Address> {
+      console.log("get address2");
+    return this.httpClient.get<Address>(this.searchLatLongUrl);
   }
 }
